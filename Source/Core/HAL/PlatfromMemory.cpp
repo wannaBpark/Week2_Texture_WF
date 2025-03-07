5c391a2a99ffe72f770e4e47ee16ea60eefa1f0c
gudtldn <gudtldn1234@gmail.com>
2025-03-07 15:41:28 +0900

    feat: 메모리 추적 클래스 추가

---
+++ Source/Core/HAL/PlatfromMemory.cpp
@@ -0,0 +1,35 @@
+﻿#include "PlatfromMemory.h"
+
+
+std::atomic<uint64> FPlatformMemory::TotalAllocationBytes = 0;
+std::atomic<uint64> FPlatformMemory::TotalAllocationCount = 0;
+
+void FPlatformMemory::IncrementStats(size_t Size)
+{
+    // TotalAllocationBytes += Size;
+    // ++TotalAllocationCount;
+
+    // 멀티스레드 대비
+    TotalAllocationBytes.fetch_add(Size, std::memory_order_relaxed);
+    TotalAllocationCount.fetch_add(1, std::memory_order_relaxed);
+}
+
+void FPlatformMemory::DecrementStats(size_t Size)
+{
+    // TotalAllocationBytes -= Size;
+    // --TotalAllocationCount;
+
+    // 멀티스레드 대비
+    TotalAllocationBytes.fetch_sub(Size, std::memory_order_relaxed);
+    TotalAllocationCount.fetch_sub(1, std::memory_order_relaxed);
+}
+
+uint64 FPlatformMemory::GetTotalAllocationBytes()
+{
+    return TotalAllocationBytes;
+}
+
+uint64 FPlatformMemory::GetTotalAllocationCount()
+{
+    return TotalAllocationCount;
+}
---
+++ Source/Core/HAL/PlatfromMemory.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+#include <atomic>
+
+#include "Core/HAL/PlatformType.h"
+
+
+/**
+ * 엔진의 Heap 메모리의 할당량을 추적하는 클래스
+ *
+ * @note new로 생성한 객체는 추적하지 않습니다.
+ */
+struct FPlatformMemory
+{
+private:
+    static std::atomic<uint64> TotalAllocationBytes;
+    static std::atomic<uint64> TotalAllocationCount;
+
+    static void IncrementStats(size_t Size);
+    static void DecrementStats(size_t Size);
+
+    /* 추적하는 클래스 목록 */
+    template <typename T, int IndexSize>
+    friend struct TContainerAllocator;
+
+public:
+    static uint64 GetTotalAllocationBytes();
+    static uint64 GetTotalAllocationCount();
+};
