461ec2e4fc9dc66006c2c28e9be7e5238c91e6ec
gudtldn <gudtldn1234@gmail.com>
2025-03-07 11:44:41 +0900

    feat: FVector 구현

---
+++ Source/Core/Math/Vector.cpp
@@ -0,0 +1,112 @@
+﻿#include "Vector.h"
+#include <cmath>
+
+
+float FVector::DotProduct(const FVector& A, const FVector& B)
+{
+    return A.X * B.X + A.Y * B.Y + A.Z * B.Z;
+}
+
+FVector FVector::CrossProduct(const FVector& A, const FVector& B)
+{
+    return {
+        A.Y * B.Z - A.Z * B.Y,
+        A.Z * B.X - A.X * B.Z,
+        A.X * B.Y - A.Y * B.X
+    };
+}
+
+float FVector::Length() const
+{
+    return sqrtf(X*X + Y*Y + Z*Z);
+}
+
+float FVector::LengthSquared() const
+{
+    return X*X + Y*Y + Z*Z;
+}
+
+FVector FVector::Normalize() const
+{
+    const float VecLength = Length();
+    return {X / VecLength, Y / VecLength, Z / VecLength};
+}
+
+float FVector::Dot(const FVector& Other) const
+{
+    return DotProduct(*this, Other);
+}
+
+FVector FVector::Cross(const FVector& Other) const
+{
+    return CrossProduct(*this, Other);
+}
+
+FVector FVector::operator+(const FVector& Other) const
+{
+    return {X + Other.X, Y + Other.Y, Z + Other.Z};
+}
+
+FVector& FVector::operator+=(const FVector& Other)
+{
+    X += Other.X; Y += Other.Y; Z += Other.Z;
+    return *this;
+}
+
+FVector FVector::operator-(const FVector& Other) const
+{
+    return {X - Other.X, Y - Other.Y, Z - Other.Z};
+}
+
+FVector& FVector::operator-=(const FVector& Other)
+{
+    X -= Other.X; Y -= Other.Y; Z -= Other.Z;
+    return *this;
+}
+
+FVector FVector::operator*(const FVector& Other) const
+{
+    return {X * Other.X, Y * Other.Y, Z * Other.Z};
+}
+
+FVector FVector::operator*(float Scalar) const
+{
+    return {X * Scalar, Y * Scalar, Z * Scalar};
+}
+
+FVector& FVector::operator*=(float Scalar)
+{
+    X *= Scalar; Y *= Scalar; Z *= Scalar;
+    return *this;
+}
+
+FVector FVector::operator/(const FVector& Other) const
+{
+    return {X / Other.X, Y / Other.Y, Z / Other.Z};
+}
+
+FVector FVector::operator/(float Scalar) const
+{
+    return {X / Scalar, Y / Scalar, Z / Scalar};
+}
+
+FVector& FVector::operator/=(float Scalar)
+{
+    X /= Scalar; Y /= Scalar; Z /= Scalar;
+    return *this;
+}
+
+FVector FVector::operator-() const
+{
+    return {-X, -Y, -Z};
+}
+
+bool FVector::operator==(const FVector& Other) const
+{
+    return X == Other.X && Y == Other.Y && Z == Other.Z;
+}
+
+bool FVector::operator!=(const FVector& Other) const
+{
+    return X != Other.X || Y != Other.Y || Z != Other.Z;
+}
---
+++ Source/Core/Math/Vector.h
@@ -0,0 +1,38 @@
+﻿#pragma once
+
+
+struct FVector
+{
+    float X, Y, Z;
+    FVector() : X(0), Y(0), Z(0) {}
+    FVector(float X, float Y, float Z) : X(X), Y(Y), Z(Z) {}
+
+    static float DotProduct(const FVector& A, const FVector& B);
+    static FVector CrossProduct(const FVector& A, const FVector& B);
+
+    float Length() const;
+    float LengthSquared() const;
+    FVector Normalize() const;
+
+    float Dot(const FVector& Other) const;
+    FVector Cross(const FVector& Other) const;
+
+    FVector operator+(const FVector& Other) const;
+    FVector& operator+=(const FVector& Other);
+
+    FVector operator-(const FVector& Other) const;
+    FVector& operator-=(const FVector& Other);
+
+    FVector operator*(const FVector& Other) const;
+    FVector operator*(float Scalar) const;
+    FVector& operator*=(float Scalar);
+
+    FVector operator/(const FVector& Other) const;
+    FVector operator/(float Scalar) const;
+    FVector& operator/=(float Scalar);
+
+    FVector operator-() const;
+
+    bool operator==(const FVector& Other) const;
+    bool operator!=(const FVector& Other) const;
+};
