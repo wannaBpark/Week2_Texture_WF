777f62aec514c2007cab37f61c9c5fb0a4e689e4
gudtldn <gudtldn1234@gmail.com>
2025-03-07 20:50:10 +0900

    feat: UEngine 클래스 추가 및 윈도우 처리 기능 구현

---
+++ Source/Core/Engine.cpp
@@ -0,0 +1,176 @@
+﻿#include "Engine.h"
+#include <iostream>
+
+// ImGui WndProc 정의
+extern LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
+
+LRESULT UEngine::WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    // ImGui의 메시지를 처리
+    if (ImGui_ImplWin32_WndProcHandler(hWnd, uMsg, wParam, lParam))
+    {
+        return true;
+    }
+
+    switch (uMsg)
+    {
+        // 창이 제거될 때 (창 닫기, Alt+F4 등)
+    case WM_DESTROY:
+        PostQuitMessage(0); // 프로그램 종료
+        break;
+
+    case WM_KEYDOWN:
+        break;
+
+    case WM_KEYUP:
+        break;
+
+    default:
+        return DefWindowProc(hWnd, uMsg, wParam, lParam);
+    }
+
+    return 0;
+}
+
+void UEngine::Initialize(
+    HINSTANCE hInstance, const WCHAR* InWindowTitle, const WCHAR* InWindowClassName, int InScreenWidth,
+    int InScreenHeight,
+    EScreenMode InScreenMode
+)
+{
+    WindowInstance = hInstance;
+    WindowTitle = InWindowTitle;
+    WindowClassName = InWindowClassName;
+    ScreenMode = InScreenMode;
+    ScreenWidth = InScreenWidth;
+    ScreenHeight = InScreenHeight;
+
+    InitWindow(InScreenWidth, InScreenWidth);
+}
+
+void UEngine::Run()
+{
+    // FPS 제한
+    constexpr int TargetFPS = 750;
+    constexpr double TargetDeltaTime = 1000.0f / TargetFPS; // 1 FPS의 목표 시간 (ms)
+
+    // 고성능 타이머 초기화
+    LARGE_INTEGER Frequency;
+    QueryPerformanceFrequency(&Frequency);
+
+    LARGE_INTEGER StartTime;
+    QueryPerformanceCounter(&StartTime);
+
+
+    IsRunning = true;
+    while (IsRunning)
+    {
+        // DeltaTime 계산 (초 단위)
+        const LARGE_INTEGER EndTime = StartTime;
+        QueryPerformanceCounter(&StartTime);
+
+        const float DeltaTime =
+            static_cast<float>(StartTime.QuadPart - EndTime.QuadPart) / static_cast<float>(Frequency.QuadPart);
+
+
+        // 메시지(이벤트) 처리
+        MSG Msg;
+        while (PeekMessage(&Msg, nullptr, 0, 0, PM_REMOVE))
+        {
+            // 키 입력 메시지를 번역
+            TranslateMessage(&Msg);
+
+            // 메시지를 등록한 Proc에 전달
+            DispatchMessage(&Msg);
+
+            if (Msg.message == WM_QUIT)
+            {
+                IsRunning = false;
+                break;
+            }
+        }
+
+
+        // TODO: Object Update Logic
+
+
+        // FPS 제한
+        double ElapsedTime;
+        do
+        {
+            Sleep(0);
+
+            LARGE_INTEGER CurrentTime;
+            QueryPerformanceCounter(&CurrentTime);
+
+            ElapsedTime = static_cast<double>(CurrentTime.QuadPart - StartTime.QuadPart) * 1000.0 / static_cast<double>(Frequency.QuadPart);
+        } while (ElapsedTime < TargetDeltaTime);
+    }
+}
+
+void UEngine::Shutdown()
+{
+    ShutdownWindow();
+}
+
+#ifdef _DEBUG
+void UEngine::OpenDebugConsole() const
+{
+    AllocConsole(); // 콘솔 창 생성
+
+    // 표준 출력 및 입력을 콘솔과 연결
+    freopen_s((FILE**)stdout, "CONOUT$", "w", stdout);
+    freopen_s((FILE**)stdin, "CONIN$", "r", stdin);
+
+    std::cout << "Debug Console Opened!" << '\n';
+}
+
+void UEngine::CloseDebugConsole() const
+{
+    FreeConsole(); // 콘솔 창 닫기
+}
+#endif
+
+void UEngine::InitWindow(int InScreenWidth, int InScreenHeight)
+{
+    // 윈도우 클래스 등록
+    WNDCLASSW wnd_class{};
+    wnd_class.lpfnWndProc = WndProc;
+    wnd_class.hInstance = WindowInstance;
+    wnd_class.lpszClassName = WindowClassName;
+    RegisterClassW(&wnd_class);
+
+    // Window Handle 생성
+    WindowHandle = CreateWindowExW(
+        0, WindowClassName, WindowTitle,
+        WS_POPUP | WS_VISIBLE | WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT,
+        InScreenWidth, InScreenHeight,
+        nullptr, nullptr, WindowInstance, nullptr
+    );
+
+    // TODO: 전체화면 구현
+    if (ScreenMode != EScreenMode::Windowed)
+    {
+        std::cout << "not implement Fullscreen and Borderless mode." << '\n';
+    }
+
+    // 윈도우 포커싱
+    ShowWindow(WindowHandle, SW_SHOW);
+    SetForegroundWindow(WindowHandle);
+    SetFocus(WindowHandle);
+
+    OpenDebugConsole();
+}
+
+void UEngine::ShutdownWindow()
+{
+    CloseDebugConsole();
+
+    DestroyWindow(WindowHandle);
+    WindowHandle = nullptr;
+
+    UnregisterClassW(WindowClassName, WindowInstance);
+    WindowInstance = nullptr;
+}
