438a8a522febadbb51d4d4cd94c0d05b3fcc4f78
gudtldn <gudtldn1234@gmail.com>
2025-03-07 15:50:44 +0900

    feat: ContainerAllocator 구현 - FPlatformMemory에서 메모리를 추적하도록 함

---
+++ Source/Core/Container/ContainerAllocator.h
@@ -0,0 +1,88 @@
+﻿#pragma once
+#include <cstdlib>
+#include <iostream>
+
+#include "Core/HAL/PlatformType.h"
+#include "Core/HAL/PlatfromMemory.h"
+
+
+struct FPlatformMemory;
+
+/**
+ * IndexSize에 따라 적절한 정수 타입을 매핑하는 메타 프로그래밍 구조체입니다.
+ * 
+ * @tparam IndexSize 지원되는 인덱스 크기 (8, 16, 32, 64 중 하나)
+ * 
+ * TBitsToSizeType은 템플릿 인자로 전달된 IndexSize 값에 따라
+ * 해당 크기에 맞는 정수 타입을 Type으로 정의합니다.
+ * 
+ * - IndexSize = 8  -> Type = int8
+ * - IndexSize = 16 -> Type = int16
+ * - IndexSize = 32 -> Type = int32
+ * - IndexSize = 64 -> Type = int64
+ * 
+ * 지원되지 않는 IndexSize를 전달하면 컴파일 타임에 static_assert를 통해 에러를 발생시킵니다.
+ */
+template <int IndexSize>
+struct TBitsToSizeType
+{
+    // ReSharper disable once CppStaticAssertFailure
+    // IndexSize가 8, 16, 32, 64중에 하나가 아니라면 컴파일 에러
+    static_assert(false, "Unsupported allocator index size.");
+};
+
+template <> struct TBitsToSizeType<8>  { using Type = int8; };
+template <> struct TBitsToSizeType<16> { using Type = int16; };
+template <> struct TBitsToSizeType<32> { using Type = int32; };
+template <> struct TBitsToSizeType<64> { using Type = int64; };
+
+
+/**
+ * Container에 사용되는 Allocator
+ * @tparam T 컨테이너 타입
+ * @tparam IndexSize 최대 Index의 크기 (bit)
+ */
+template <typename T, int IndexSize>
+struct TContainerAllocator
+{
+public:
+    using SizeType = typename TBitsToSizeType<IndexSize>::Type;
+
+    //~ std::allocator_traits 관련 타입
+    using value_type = T;
+    using size_type = std::make_unsigned_t<SizeType>;
+    using difference_type = std::make_signed_t<SizeType>;
+    using propagate_on_container_move_assignment = std::true_type;
+    using is_always_equal = std::true_type;
+
+    template <typename U>
+    struct rebind
+    {
+        using other = TContainerAllocator<U, IndexSize>;
+    };
+    //~ std::allocator_traits 관련 타입
+
+public:
+    T* allocate(size_type n);
+    void deallocate(T* p, size_type n) noexcept;
+};
+
+template <typename T, int IndexSize>
+T* TContainerAllocator<T, IndexSize>::allocate(size_type n)
+{
+    std::cout << "FContainerAllocator<T, IndexSize>::allocate() " << static_cast<uint64>(n) << "bytes\n";
+    const size_t AllocSize = sizeof(T) * n;
+    FPlatformMemory::IncrementStats(AllocSize);
+    return static_cast<T*>(std::malloc(AllocSize));
+}
+
+template <typename T, int IndexSize>
+void TContainerAllocator<T, IndexSize>::deallocate(T* p, size_type n) noexcept
+{
+    std::cout << "FContainerAllocator<T, IndexSize>::deallocate() " << static_cast<uint64>(n) << " bytes\n";
+    FPlatformMemory::DecrementStats(sizeof(T) * n);
+    std::free(p);
+}
+
+template <typename T> using FDefaultAllocator = TContainerAllocator<T, 32>;
+template <typename T> using FDefaultAllocator64 = TContainerAllocator<T, 64>;
