c46a964c51af0ad28c958fb5a1853c0e79234145
gudtldn <gudtldn1234@gmail.com>
2025-03-07 19:47:43 +0900

    wip: String 초기구현 추가

---
+++ Source/Core/Container/String.cpp
@@ -0,0 +1,7 @@
+﻿#include "String.h"
+
+
+FString FString::FromInt(int32 Num)
+{
+    return FString{std::to_string(Num)};
+}
---
+++ Source/Core/Container/String.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+#include <string>
+
+#include "ContainerAllocator.h"
+
+
+class FString
+{
+private:
+    using BaseStringType = TCHAR;
+    std::basic_string<
+        BaseStringType,
+        std::char_traits<BaseStringType>,
+        FDefaultAllocator<BaseStringType>
+    > PrivateString;
+
+    explicit FString(const std::string& InString) : PrivateString(InString) {}
+
+public:
+    FString() : PrivateString() {}
+    FString(const ANSICHAR* InString) : PrivateString(InString) {}
+    
+    static FString FromInt(int32 Num);
+
+public:
+    // bool Equals(const FString& Other) const;
+};
