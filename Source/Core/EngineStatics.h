034d01267716389106f4d567fc6937c3cb84635e
gudtldn <gudtldn1234@gmail.com>
2025-03-07 19:47:12 +0900

    feat: EngineStatics 추가

---
+++ Source/Core/EngineStatics.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+#include "HAL/PlatformType.h"
+
+
+class UEngineStatics
+{
+public:
+    static uint32 GenUUID()
+    {
+        return NextUUID++;
+    }
+
+    static uint32 NextUUID;
+};
