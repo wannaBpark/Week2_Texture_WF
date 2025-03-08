8c08f52931cbc05aa075daa16490fd0582cdf09f
gudtldn <gudtldn1234@gmail.com>
2025-03-08 12:52:11 +0900

    feat: 메모리 할당 및 해제 함수 추가 및 통계 관리 로직 개선

--- Source/Core/HAL/PlatformMemory.h
+++ Source/Core/HAL/PlatformMemory.h
@@ -1,6 +1,5 @@
 ﻿#pragma once
 #include <atomic>
-
 #include "Core/HAL/PlatformType.h"
 
 
@@ -15,18 +14,38 @@ private:
     static std::atomic<uint64> TotalAllocationBytes;
     static std::atomic<uint64> TotalAllocationCount;
 
-    static void IncrementStats(size_t Size);
-    static void DecrementStats(size_t Size);
+    static void IncrementStats(size_t Size)
+    {
+        // TotalAllocationBytes += Size;
+        // ++TotalAllocationCount;
 
-    /* 추적하는 클래스 목록 */
-    template <typename T, int IndexSize>
-    friend struct TContainerAllocator;
+        // 멀티스레드 대비
+        TotalAllocationBytes.fetch_add(Size, std::memory_order_relaxed);
+        TotalAllocationCount.fetch_add(1, std::memory_order_relaxed);
+    }
 
-    friend class FObjectFactory;
+    static void DecrementStats(size_t Size)
+    {
+        // TotalAllocationBytes -= Size;
+        // --TotalAllocationCount;
+
+        // 멀티스레드 대비
+        TotalAllocationBytes.fetch_sub(Size, std::memory_order_relaxed);
+        TotalAllocationCount.fetch_sub(1, std::memory_order_relaxed);
+    }
 
 public:
-    static uint64 GetTotalAllocationBytes();
-    static uint64 GetTotalAllocationCount();
+    static uint64 GetTotalAllocationBytes()
+    {
+        return TotalAllocationBytes;
+    }
 
+    static uint64 GetTotalAllocationCount()
+    {
+        return TotalAllocationCount;
+    }
 
+    static void* Malloc(size_t Size);
+    static void* AlignedMalloc(size_t Size, size_t Alignment);
+    static void Free(void* Address, size_t Size);
 };
