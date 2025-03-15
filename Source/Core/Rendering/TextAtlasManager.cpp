4c0411b0e94f49a4f529aff55472a99b857fecaa
chanhuiHan <hch3245@naver.com>
2025-03-15 17:24:54 +0900

    fix: Get의 반환 방식이 pos, offset 으로 되도록, Static 함수로하도록

--- Source/Core/Rendering/TextAtlasManager.cpp
+++ Source/Core/Rendering/TextAtlasManager.cpp
@@ -1,5 +1,6 @@
 #include "TextAtlasManager.h"
 #include "Debug/DebugConsole.h"
+#include "Core/Math/Vector.h"
 
 UTextAtlasManager::UTextAtlasManager()
 	: AtlasWidth(0), AtlasHeight(0), Columns(0), Rows(0),
@@ -8,34 +9,34 @@ UTextAtlasManager::UTextAtlasManager()
 
 }
 
-void UTextAtlasManager::SetAtlasManager(int InWidth, int InHeight, int InColumns, int InRows, int InMarginX, int InMarginY)
+static void UTextAtlasManager::SetAtlasManager(int InWidth, int InHeight, int InColumns, int InRows, int InMarginX, int InMarginY)
 {
 	SetAtlasSize(InWidth, InHeight);
 	SetGridLayout(InColumns, InRows);
 	SetMargin(InMarginX, InMarginY);
 }
 
-void UTextAtlasManager::SetAtlasSize(int InWidth, int InHeight)
+static void UTextAtlasManager::SetAtlasSize(int InWidth, int InHeight)
 {
 	AtlasWidth = InWidth;
 	AtlasHeight = InHeight;
 }
 
-void UTextAtlasManager::SetGridLayout(int InColumns, int InRows)
+static void UTextAtlasManager::SetGridLayout(int InColumns, int InRows)
 {
 	Columns = InColumns;
 	Rows = InRows;
 }
 
-void UTextAtlasManager::SetMargin(int InMarginX, int InMarginY)
+static void UTextAtlasManager::SetMargin(int InMarginX, int InMarginY)
 {
 	MarginX = InMarginX;
 	MarginY = InMarginY;
 }
 
-UVRect UTextAtlasManager::GetCharUV(char character) const
+FVector4 UTextAtlasManager::GetCharUV(char character) const
 {
-    UVRect rect = { 0.f, 0.f, 0.f, 0.f };
+    FVector4 rect = { 0.f, 0.f, 0.f, 0.f };
     if (character > Columns * Rows - 1 || Columns <= 0 || Rows <= 0 || AtlasWidth <= 0 || AtlasHeight <= 0)
         UE_LOG("TextAtlasManager GetCharUV Error!");
         return rect; // 잘못된 입력이면 기본값 반환
@@ -59,10 +60,10 @@ UVRect UTextAtlasManager::GetCharUV(char character) const
     int y = row * (cellHeight + MarginY);
 
     // 정규화된 UV 좌표 계산 (0.0 ~ 1.0)
-    rect.u0 = static_cast<float>(x) / AtlasWidth;
-    rect.v0 = static_cast<float>(y) / AtlasHeight;
-    rect.u1 = static_cast<float>(x + cellWidth) / AtlasWidth;
-    rect.v1 = static_cast<float>(y + cellHeight) / AtlasHeight;
+    rect.X = static_cast<float>(cellWidth);
+    rect.Y = static_cast<float>(cellHeight);
+    rect.Z = static_cast<float>(x) / AtlasWidth;
+    rect.W = static_cast<float>(y) / AtlasHeight;
 
     return rect;
 }
